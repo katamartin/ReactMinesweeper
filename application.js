bce8908822ff5ba40a0ca05166b0ac2fed00914b
katamartin <katasm@gmail.com>
2015-10-05 16:21:29 -0700

    add basic game methods

--- application.js
+++ application.js
@@ -1,3 +1,19 @@
 (function(React) {
   "use strict";
+  var Game = React.createClass({
+    getInitialState: function() {
+      var board = new Minesweeper.Board(10, 10);
+      return {board: board, won: false, over: false};
+    },
+
+    updateGame: function() {
+
+    },
+
+    render: function() {
+      return(
+        <Board board={this.state.board} updateGame={this.updateGame}/>
+      );
+    }
+  });
 }(window.React));
