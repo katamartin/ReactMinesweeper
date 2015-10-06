466040273fe9a23baebd84434f074fd41b78c9bb
katamartin <katasm@gmail.com>
2015-10-05 17:02:29 -0700

    implement board render method with trivial tile render

--- application.js
+++ application.js
@@ -1,5 +1,36 @@
 (function(React) {
   "use strict";
+  var Tile = React.createClass({
+    render: function() {
+      return(<div>" T "</div>);
+    }
+  });
+
+  var Board = React.createClass({
+    render: function() {
+      var that = this;
+      return (
+        <div>
+        {
+          that.props.board.grid.map(function(row, i) {
+            return (
+              <div key={i}>
+              {
+                row.map(function(tile, j) {
+                  return (
+                    <Tile position={tile.pos} updateGame={that.updateGame} key={[i,j]}/>
+                  );
+                })
+              }
+              </div>
+            );
+          })
+        }
+        </div>
+      )
+    }
+  });
+
   var Game = React.createClass({
     getInitialState: function() {
       var board = new Minesweeper.Board(10, 10);
@@ -16,4 +47,9 @@
       );
     }
   });
+
+  React.render(
+    <Game />,
+    document.getElementById('content')
+  );
 }(window.React));
